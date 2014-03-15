a68bbd8b00dde6ea9f6c6fc9db060d6916917999
dejavusv <surachai.ns@gmail.com>
2014-03-15 10:05:40 +0700

    Update PIRPlugin.java

--- src/android/PIRPlugin.java
+++ src/android/PIRPlugin.java
@@ -18,7 +18,6 @@ public class PIRPlugin extends CordovaPlugin{
 
 	private Context thisContext;
 	private Intent ioioService;
-	private Intent broadcastIntent = new Intent("msgIOIO");
 	private String interval="";
 	private String dulation="";
 	public int PIRDetect =2;
@@ -30,78 +29,14 @@ public class PIRPlugin extends CordovaPlugin{
 			final CallbackContext callbackContext) throws JSONException {
 
 		if (action.equals("startservice")) {
-		//	WriteFile write = new WriteFile();
-            	//	String data = write.writeToFile("Hello world");
 			System.out.println("startup IOIO service");
 			startService(new Intent(this,MyService.class));
 			finish();
 			callbackContext.success("data");
-            	//	this.ioioStartup(callbackContext); 
             return true;
         }
-		if (action.equals("ioiogetdata")) {
-            this.ioioGetdata(callbackContext); 
-            return true;
-        }
-        if (action.equals("ioioSendMessage")) {
-            this.ioioSendMessage(args.getString(0),args.getString(1),args.getString(2) ,callbackContext); 
-            return true;
-        }        
+	
         return false;
 	}
-
-    // Initialise IOIO service (Called from Javascript)
-    private void ioioStartup(CallbackContext callbackContext) {
-    	// Initialise the service variables and start it it up
-    	try{
-    	thisContext = this.cordova.getActivity().getApplicationContext();
-    	ioioService = new Intent(thisContext, PIRProvider.class);
-        thisContext.startService(ioioService); 
-        System.out.println("start service");
-        
-        callbackContext.success("status up");
-        
-       			                	 	
-        }catch(Exception e){
-        	callbackContext.success("status error :"+e.toString());
-        }
-
-    	
-    }
-    
-    private void ioioGetdata(CallbackContext callbackContext) {
-    	String message = String.valueOf(PIRDetect);
-    //	System.out.println("PIR Detect :"+message);
-    	
-
-    	if (message != null && message.length() > 0) { 
-            //callbackContext.success(message);
-            
-            this.connectionCallbackMotion = callbackContext;
-    		PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-        	pluginResult.setKeepCallback(true);
-        	callbackContext.sendPluginResult(pluginResult);
-        	cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                	while(true){
-                		PluginResult result = new PluginResult(PluginResult.Status.OK, String.valueOf(PIRDetect));
-                    	result.setKeepCallback(true);
-                    	connectionCallbackMotion.sendPluginResult(result);
-                		try{
-                			Thread.sleep(300);
-                		}catch(Exception ex){
-                			ex.printStackTrace();
-                		}
-                	}
-                }
-            });
-        	//return true;
-        } else {
-            callbackContext.error("IOIO.java Expected one non-empty string argument.");
-        }
-    }
-    
-
-    
 
 }
